5c7f1a349d6673e305da2502d55b192ac86feab6
Zuzanna <zuzanna.misztal.agh@gmail.com>
2020-07-30 16:04:55 +0200

    load_products first attempt

--- vending.py
+++ vending.py
@@ -3,34 +3,34 @@ from dataclasses import dataclass
 from decimal import Decimal
 import typing
 
-ProductName = str
-Slot_code = str
+ProductName = typing.NewType('ProductName', str)
+SlotCode = typing.NewType('SlotCode', str)
 Assortment = typing.Dict[ProductName, 'Product']
 Coins = Counter[Decimal, int]
-Menu = typing.Dict[ProductName, typing.Tuple[Slot_code, Decimal]]
+Menu = typing.Dict[ProductName, typing.Tuple[SlotCode, Decimal]]
 
 
 class MachineOverloadedException(Exception):
     pass
 
 
-class ProductUnavailableException(Exception):
-    pass
-
-
 @dataclass
 class Product:
-    name: str
+    name: ProductName
     quantity: int
     price: Decimal
 
 
 class Machine:
     def __init__(self, slots: int, slot_depth: int) -> None:
-        pass
+        self.coins = Coins()
+        self.slot_depth = slot_depth
+        self.slots: typing.Dict[SlotCode, Product] = {str(i): None for i in range(slots)}
 
     def load_products(self, assortment: Assortment) -> None:
-        pass
+        for product_name, product in assortment.items():
+            if product_name in [product.name for product in self.slots.values()]:
+                pass
 
     def load_coins(self, coins: Coins) -> None:
         pass
@@ -38,7 +38,7 @@ class Machine:
     def get_available_products(self) -> Menu:
         pass
 
-    def choose_product(self, product_code: str, money: Coins) -> typing.Tuple[typing.Optional[Product], typing.Optional[Coins]]:
+    def choose_product(self, product_code: SlotCode, money: Coins) -> typing.Tuple[typing.Optional[Product], typing.Optional[Coins]]:
         pass
 
     def get_balance(self) -> Decimal:
