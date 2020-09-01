7474bae00a2e5e5fb770826d06dffd208276264d
Zuzanna <zuzanna.misztal.agh@gmail.com>
2020-09-01 19:28:54 +0200

    Changed _get_change method

--- vending.py
+++ vending.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from dataclasses import dataclass
 from decimal import Decimal
 from math import ceil
 import typing
@@ -12,13 +11,10 @@ Menu = typing.Dict[ProductName, typing.Tuple[SlotCode, Decimal]]
 
 
 def sum_of_coins(coins: Coins) -> Decimal:
-    sum: Decimal = Decimal(0)
-    for nominal, quantity in coins.most_common():
-        sum += nominal * quantity
-    return sum
+    return sum([nominal * quantity for nominal, quantity in coins.items()])
 
 
-def add_assortments(assortment1: Assortment, assortment2: Assortment) -> Assortment:
+def add_assortments(assortment1: typing.Optional[Assortment], assortment2: Assortment) -> Assortment:
     if assortment1 is None:
         return assortment2
     result = {}
@@ -32,6 +28,24 @@ def add_assortments(assortment1: Assortment, assortment2: Assortment) -> Assortm
             continue
         result[product_name] = assortment2[product_name]
     return result
+
+
+def collect_change(coins_available: Coins, value_to_collect: Decimal, change: Coins) -> typing.Optional[Coins]:
+    if value_to_collect == 0:
+        return change
+    if value_to_collect < 0:
+        return None
+    for coin in coins_available:
+        new_coins_available = coins_available.copy()
+        new_coins_available[coin] -= 1
+        new_coins_available += Counter()
+        new_change = change.copy()
+        new_change.update({coin: 1})
+        new_value_to_collect = value_to_collect - coin
+        ret = collect_change(new_coins_available, new_value_to_collect, new_change)
+        if ret:
+            return ret
+    return None
 
 
 class MachineOverloadedException(Exception):
@@ -92,18 +106,18 @@ class Machine:
         return items
 
     def choose_product(self, product_code: SlotCode, money: Coins) -> typing.Tuple[typing.Optional[Product], typing.Optional[Coins]]:
-        if product_code not in self.slots.keys():
+        if product_code not in self.slots:
             return None, money
-        slot = self.slots[product_code]
-        if slot is None or slot.quantity == 0 or sum_of_coins(money) < slot.price:
+        product = self.slots[product_code]
+        if product is None or product.quantity == 0 or sum_of_coins(money) < product.price:
             return None, money
         self.load_coins(money)
         try:
-            change: Coins = self._get_change(money, slot.price)
+            change: Coins = self._get_change(money, product.price)
             self._give_coins(change)
-            slot.quantity -= 1
-            self.assortment[slot.name].quantity -= 1
-            return Product(slot.name, 1, slot.price), change
+            product.quantity -= 1
+            self.assortment[product.name].quantity -= 1
+            return Product(product.name, 1, product.price), change
         except NotEnoughMoneyInMachineException:
             self._give_coins(money)
             return None, money
@@ -114,19 +128,13 @@ class Machine:
     def cash_out(self) -> Coins:
         pass
 
-    def _get_change(self, money: Coins, price: Decimal) -> Coins:
+    def _get_change(self, money: Coins, price: Decimal) -> typing.Optional[Coins]:
         if sum_of_coins(money) == price:
             return None
-        to_return = Coins()
-        amount_of_money_yet_to_return = sum_of_coins(money) - price
-        for nominal, quantity in sorted(self.coins.items(), reverse=True):
-            quantity_to_give: int = min(amount_of_money_yet_to_return // nominal, quantity)
-            if quantity_to_give > 0:
-                to_return[nominal] = quantity_to_give
-                amount_of_money_yet_to_return -= quantity_to_give * nominal
-            if amount_of_money_yet_to_return == 0:
-                return to_return
-        raise NotEnoughMoneyInMachineException
+        change = collect_change(self.coins, sum_of_coins(money) - price, Counter())
+        if not change:
+            raise NotEnoughMoneyInMachineException
+        return change
 
     def _give_coins(self, change: Coins) -> None:
         if change is None:
@@ -135,4 +143,3 @@ class Machine:
             if self.coins[nominal] < quantity:
                 raise NotEnoughMoneyInMachineException
             self.coins[nominal] -= quantity
-
